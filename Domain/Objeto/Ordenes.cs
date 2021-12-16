044a078c8c986b5ea346a29b551ad22136a57f7a
KennethT2014 <54214846+KennethT2014@users.noreply.github.com>
2021-12-16 09:14:42 -0800

    Update Ordenes.cs

--- Domain/Objeto/Ordenes.cs
+++ Domain/Objeto/Ordenes.cs
@@ -11,28 +11,5 @@ namespace Domain.Objeto
         public Enum Ciudad { get; set; }
         public DateTime fechaOrden { get; set; }
 
-        public class OrdenesPaisComparer : IComparer<Ordenes>
-        {
-            public int Compare(Ordenes e1, Ordenes e2)
-            {
-                if (e1 == null || e2 == null)
-                {
-                    throw new ArgumentException("Error, debe seleccionar un país");
-                }
-                if(e1.Pais > e2.Pais)
-                {
-                    return 1;
-                }
-                else if(e1.Pais < e2.Pais)
-                {
-                    return -1;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
-
     }
 }
